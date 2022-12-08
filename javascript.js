389ee3dcd4ff569739ed1f3ca319d89f067e8f5b
Majid-pkz <majid.computing@gmail.com>
2022-12-09 01:33:03 +1100

    added next button and used only two Arrays- one for questions and one for Answers.index in answer array grow by +4

--- javascript.js
+++ javascript.js
@@ -2,10 +2,12 @@ var timer;
 var timeEl=document.querySelector(".time");
 var timeCounter=60;
 var timeOverMsg=document.querySelector("#timeOver-message");
-var startButton=document.querySelector(".start-button"); 
+var startButton=document.querySelector(".start-button");
+var nextButton=document.querySelector(".next-button"); 
 var questionEl=document.querySelector(".question-place")
 var numbQuestion=0;
 var currentQuestion=0;
+var choices=0;
 
 var chiceA=document.querySelector("#a");
 var chiceB=document.querySelector("#b");
@@ -15,10 +17,14 @@ var chiceD=document.querySelector("#d");
 
 
 var questions=["1-What is Javascript","2-What is API","3-What is JSON"]
-var answ1Options=["A- browsers programming lang","B- databse","C- Anti-Virus","D- DocumentScript"];
-var correctAns1 ="browsers programming lang"
-var answ2Options=["Australian Property Institute","Application Programming Interface","Android Applications","None of the above"];
-var answ3Optiona=["Computer manufacturing","Firewall","Javascript Object Notation","None"];
+var answOptions=["A- browsers programming lang","B- databse","C- Anti-Virus","D- DocumentScript",
+"A- Australian Property Institute","B- Application Programming Interface","C- Android Applications","D- None of the above",
+"Computer manufacturing","Firewall","Javascript Object Notation","None",
+"AQ4","BQ4","CQ4","DQ4"
+];
+// var correctAns1 ="browsers programming lang"
+// var answ2Options=["A- Australian Property Institute","B- Application Programming Interface","C- Android Applications","D- None of the above"];
+// var answ3Optiona=["Computer manufacturing","Firewall","Javascript Object Notation","None"];
 
 timeCounter.textContent=timer;
 
@@ -54,13 +60,21 @@ function displayQusetion(){
     questionEl.textContent=questions[currentQuestion]
 
 }
+function nextQuestion(){
+    //add one to question array index
+    currentQuestion++;
+    //add 4 to answers array index to move to next series of choices
+    choices=choices+4;
+    displayQusetion();
+    displayAnswer();
+}
 
 
-function displayAnswer(choices){
-    chiceA.textContent=answ1Options[0];
-    chiceB.textContent=answ1Options[1];
-    chiceC.textContent=answ1Options[2];
-    chiceD.textContent=answ1Options[3];
+function displayAnswer(){
+    chiceA.textContent=answOptions[choices];
+    chiceB.textContent=answOptions[choices+1];
+    chiceC.textContent=answOptions[choices+2];
+    chiceD.textContent=answOptions[choices+3];
 
 }
 
@@ -68,5 +82,5 @@ function displayAnswer(choices){
 
 
 startButton.addEventListener("click",startQuiz);
-
+nextButton.addEventListener("click",nextQuestion)
 
