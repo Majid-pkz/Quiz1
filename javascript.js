77db790ab8550fe5463846bfecdbe8132e5f1114
Majid-pkz <majid.computing@gmail.com>
2022-12-09 16:15:51 +1100

    Added CheckAnswer function with feedback(coorect/wrong)

--- javascript.js
+++ javascript.js
@@ -5,9 +5,21 @@ var timeOverMsg=document.querySelector("#timeOver-message");
 var startButton=document.querySelector(".start-button");
 var nextButton=document.querySelector(".next-button"); 
 var questionEl=document.querySelector(".question-place")
+var selectA=document.querySelector("#a");
+var selectB=document.querySelector("#b")
+var selectC=document.querySelector("#c")
+var selectD=document.querySelector("#d")
+var feedbackEL=document.querySelector("#feedback")
+ var currentA="";
+ var currentB="";
+ var currentC="";
+ var currentD="";
+
 var numbQuestion=0;
 var currentQuestion=0;
 var choices=0;
+var feedback="";
+
 
 var chiceA=document.querySelector("#a");
 var chiceB=document.querySelector("#b");
@@ -16,12 +28,13 @@ var chiceD=document.querySelector("#d");
 
 
 
-var questions=["1-What is Javascript","2-What is API","3-What is JSON"]
+var questions=["1-What is Javascript","2-What is API","3-What is JSON","4-what was your favorite lecture"]
 var answOptions=["A- browsers programming lang","B- databse","C- Anti-Virus","D- DocumentScript",
 "A- Australian Property Institute","B- Application Programming Interface","C- Android Applications","D- None of the above",
 "Computer manufacturing","Firewall","Javascript Object Notation","None",
-"AQ4","BQ4","CQ4","DQ4"
+"AQ4","BQ4","CQ4-Myfavorite","DQ4"
 ];
+var correctAnswer=["A- browsers programming lang","B- Application Programming Interface","Javascript Object Notation","CQ4-Myfavorite"];
 // var correctAns1 ="browsers programming lang"
 // var answ2Options=["A- Australian Property Institute","B- Application Programming Interface","C- Android Applications","D- None of the above"];
 // var answ3Optiona=["Computer manufacturing","Firewall","Javascript Object Notation","None"];
@@ -32,6 +45,7 @@ function startQuiz(){
     startTimer();
     displayQusetion();
     displayAnswer();
+    
 }
 
 function startTimer(){
@@ -57,8 +71,7 @@ function timeOverMesage(){
 }
 
 function displayQusetion(){
-    questionEl.textContent=questions[currentQuestion]
-
+    questionEl.textContent=questions[currentQuestion];
 }
 function nextQuestion(){
     //add one to question array index
@@ -69,18 +82,71 @@ function nextQuestion(){
     displayAnswer();
 }
 
-
 function displayAnswer(){
     chiceA.textContent=answOptions[choices];
+    currentA=answOptions[choices];
     chiceB.textContent=answOptions[choices+1];
+    currentB=answOptions[choices+1];
     chiceC.textContent=answOptions[choices+2];
+    currentC=answOptions[choices+2];
     chiceD.textContent=answOptions[choices+3];
+    currentD=answOptions[choices+3];
+}
+function checkAnswerforA(){
+    if(correctAnswer.includes(currentA)){
+    feedback="correct";
+    }
+    else{
+        feedback="Wrong";
+        timeCounter-=20;
+    }
+    feedbackEL.textContent=feedback;
+    nextQuestion();
+}
+
+function checkAnswerforB(){
+    if(correctAnswer.includes(currentB)){
+        feedback="correct";
+      }
+      else{
+          feedback="Wrong";
+          timeCounter-=20;
+      }
+      feedbackEL.textContent=feedback;
+      nextQuestion();
+
+}
+function checkAnswerforC(){
+    if(correctAnswer.includes(currentC)){
+        feedback="correct";
+      }
+      else{
+          feedback="Wrong";
+          timeCounter-=20;
+      }
+      feedbackEL.textContent=feedback;
+      nextQuestion();
+
+}
+function checkAnswerforD(){
+    if(correctAnswer.includes(currentD)){
+        feedback="correct";
+      }
+      else{
+          feedback="Wrong";
+          timeCounter-=20;
+      }
+      feedbackEL.textContent=feedback;
+      nextQuestion();
 
 }
 
 
-
-
 startButton.addEventListener("click",startQuiz);
 nextButton.addEventListener("click",nextQuestion)
+selectA.addEventListener("click",checkAnswerforA,nextQuestion);
+selectB.addEventListener("click",checkAnswerforB,nextQuestion);
+selectC.addEventListener("click",checkAnswerforC,nextQuestion);
+selectD.addEventListener("click",checkAnswerforD,nextQuestion)
 
+console.log(currentA);
